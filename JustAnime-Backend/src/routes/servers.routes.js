ea9e8d2e59710c54595d2be97b83106498b4ac41
coochieslayer9000 <umarfarhan987@gmail.com>
2025-12-07 14:07:27 +0500

    Delete JustAnime-Backend/src/routes directory

--- JustAnime-Backend/src/routes/servers.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getServers } from "../controllers/servers.controller.js";
-
-const router = express.Router();
-
-router.get("/servers", getServers);
-
-export default router;
